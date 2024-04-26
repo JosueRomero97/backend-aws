3d768b4c7d15802ffc40826ff48c6c79744156bc
JosueRomero97 <josueromero1901@gmail.com>
2024-04-26 15:57:44 +0000

    updateCrudUser

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,59 +1,74 @@
 
-import { readAllUsers,createUpdateUser,getUserById,deleteUser } from "../db/db.user.js"
+import { readAllUsers, createUpdateUser, getUserById, deleteUser } from "../db/db.user.js"
 
-export const listaUsuarios = async (req,res)=>{
+export const listaUsuarios = async (req, res) => {
     try {
         const users = await readAllUsers()
         if (users.success) {
             console.log('Datos obtenidos:', users.data);
-            return res.status(200).json({success:users.success,data:users.data});
+            return res.status(200).json({ success: users.success, data: users.data });
         } else {
             // Si no se obtienen datos, devuelve un error 404
             return res.status(404).json({ message: 'No se encontraron usuarios.' });
         }
     } catch (error) {
-        return res.status(500).json( {messsage: 'error'});
-    }  
+        return res.status(500).json({ messsage: 'error' });
+    }
 }
 
 
-export const crearUsuario = async (req,res)=>{
+export const crearUsuario = async (req, res) => {
     try {
-        console.log('body',req.body)
-        console.log('dni',req.body.dni)
-        //validar si existe usuario
-        const validar = await getUserById(req.body.dni);
-        console.log('validando',validar.success)
-        if(validar.success){
-            return res.status(200).json({success:false,data:'Usuario ya existe'})
+        console.log('body', req.body)
+        console.log('dni', req.body.data.dni)
+        //CREAR
+        if (req.body.tipo === 0) {
+            const validar = await getUserById(req.body.data.dni);
+            // console.log('validando', validar.success)
+            if (validar.success) {
+                return res.status(200).json({ success: false, data: 'Usuario ya existe' })
+            }
+            const { success, data } = await createUpdateUser(req.body.data)
+            return res.json({ success, data })
         }
-        const {success,data} = await createUpdateUser(req.body)
-        return res.json({success,data})
+
+        //ACTUALIZAR
+        if (req.body.tipo === 1) {
+            const validar = await getUserById(req.body.data.dni);
+            console.log('validando', validar.success)
+            if (validar.success) {
+                const { success, data } = await createUpdateUser(req.body.data)
+                return res.json({ success, data:'Usuario actualizado exitosamente' })
+            }
+            return res.status(200).json({ success: false, data: 'Usuario no encontrado' })
+        }
+        return res.status(200).json({ success: false, data: 'Opción no válida' })
+
     } catch (error) {
-        return res.status(500).json({messsage:'error'})
+        return res.status(500).json({ messsage: 'error' })
     }
 }
 
 
-export const obtenerUsuario = async (req,res)=>{
+export const obtenerUsuario = async (req, res) => {
     try {
-        const {dni} = req.params
-        const {success,data}=await getUserById(dni);
-        
-        return res.status(200).json({success,data})
+        const { dni } = req.params
+        const { success, data } = await getUserById(dni);
+
+        return res.status(200).json({ success, data })
     } catch (error) {
         console.log(error)
-        return res.status(500).json({messsage:'error'})
+        return res.status(500).json({ messsage: 'error' })
     }
 }
 
-export const eliminarUsuario = async (req,res)=>{
+export const eliminarUsuario = async (req, res) => {
     try {
-        const {dni}=req.params;
-        const {success} = await deleteUser(dni);
-        return res.status(200).json({success})
+        const { dni } = req.params;
+        const { success } = await deleteUser(dni);
+        return res.status(200).json({ success })
     } catch (error) {
         console.log(error)
-        return res.status(500).json({message:'error'})
+        return res.status(500).json({ message: 'error' })
     }
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,7 @@ import * as userCtrl  from '../controller/user.controller.js'
 const router = Router();
 
 router.get('/list',userCtrl.listaUsuarios);
-router.post('/create',userCtrl.crearUsuario);
+router.post('/',userCtrl.crearUsuario);
 router.get('/:dni',userCtrl.obtenerUsuario);
 router.delete('/:dni',userCtrl.eliminarUsuario);
 export default router;
